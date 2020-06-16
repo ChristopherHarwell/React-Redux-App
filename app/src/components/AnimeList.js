fa4c019b2527b7b13f6847046971312d9860f31d
Chris <chrisharwell72@gmail.com>
2020-06-15 23:20:45 -0400

    added some styling with material UI

--- app/src/components/AnimeList.js
+++ app/src/components/AnimeList.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
 
-const AnimeList = props => {
-    return (
-        <>
-            <h2>Welcome To Studio Ghibli</h2>
-            <div>
-                {props.films.map(film => (
-                    <h4 key={film.id}>{film.title}</h4>
-                ))}
-            </div>
-        </>
-    )
-}
+const AnimeList = (props) => {
+  return (
+    <>
+      <h2>Welcome To Studio Ghibli</h2>
+      <div>
+        {props.films.map((film) => (
+          <Card key={film.id} variant="outlined">
+            <CardContent>
+              <Typography variant="h5" gutterBottom>
+                {film.title}
+              </Typography>
+              <span>
+                Description:
+                <Typography variant="body2"> {film.description}</Typography>
+              </span>
+              <span>
+                Director:
+                <Typography variant="body2"> {film.director}</Typography>
+              </span>
+              <span>
+                Producer:
+                <Typography variant="body2"> {film.producer}</Typography>
+              </span>
+              <span>
+                Release Date:
+                <Typography variant="body2"> {film.release_date}</Typography>
+              </span>
+              <span>
+                Rotten Tomatoes Score:
+                <Typography variant="body2"> {film.rt_score}</Typography>
+              </span>
+              
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default AnimeList
+export default AnimeList;
